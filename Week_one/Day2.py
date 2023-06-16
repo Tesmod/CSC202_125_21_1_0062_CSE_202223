f2d9cbadafd02933d9f31ad17bdafaf000f1ff02
Tesmod <agboolafarouq11@gmail.com>
2023-06-16 19:39:13 +0100

    complete Day2

--- Week_one/Day2.py
+++ Week_one/Day2.py
@@ -1,30 +1,30 @@
-# # # #Day 2.1 Challenge
-# # # num = input("Enter any two digit number: ")
-# # # first = int(num[0])
-# # # second = int(num[1])
-# # # add = first + second
-# # # print(add)
+#Day 2.1 Challenge
+num = input("Enter any two digit number: ")
+first = int(num[0])
+second = int(num[1])
+add = first + second
+print(add)
 
-# # #Mathematical operators PEMDAS
-# # # + - * / **
+#Mathematical operators PEMDAS
+# + - * / **
 
-# # #BMI Exercise
-# # weight = int(input("Enter your weight in kg: "))
-# # height = float(input("Enter your height in m: "))
-# # square_height = height ** 2
-# # bmi = int(weight / square_height)
-# # print("Your Body Mass Index is ", bmi)
+#BMI Exercise
+weight = int(input("Enter your weight in kg: "))
+height = float(input("Enter your height in m: "))
+square_height = height ** 2
+bmi = int(weight / square_height)
+print("Your Body Mass Index is ", bmi)
 
-# #Rounding Numbers
-# print(round(8 / 3, 5))
+#Rounding Numbers
+print(round(8 / 3, 5))
 
-# #Exercise One
-# age = input("What is your current age? ")
-# years = 90 - int(age)
-# days = 365 *  int(years)
-# weeks = 52 * int(years)
-# months = 12 * int(years)
-# print(f"You have {days} days, {weeks} weeks and {months} months left ")
+#Exercise One
+age = input("What is your current age? ")
+years = 90 - int(age)
+days = 365 *  int(years)
+weeks = 52 * int(years)
+months = 12 * int(years)
+print(f"You have {days} days, {weeks} weeks and {months} months left ")
 
 #Exercise - Tip Calculator
 print("Welcome to the Tip Calculator")
