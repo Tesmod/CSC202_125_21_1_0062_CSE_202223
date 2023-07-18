ba0efca18aadc95a03663b65f116173dbaa009db
Tesmod <agboolafarouq11@gmail.com>
2023-07-18 15:36:17 +0100

    Addded Day16 and Day17 files

---
+++ Chunin/Week_three/Day17.py
@@ -0,0 +1,12 @@
+#Creating a class in python
+class User:
+
+
+    def __init__(self, user.id, username):
+        self.id  = user.id
+        self.username = username
+    
+user_1 = User() 
+
+print(user_1.username)
+
