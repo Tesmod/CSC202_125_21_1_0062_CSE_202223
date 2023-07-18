5ddcfd0ceabf94550ea8d5867600b74bd7b6e94d
Tesmod <agboolafarouq11@gmail.com>
2023-07-18 15:44:36 +0100

    Finished Day 17

--- Chunin/Week_three/Day17.py
+++ Chunin/Week_three/Day17.py
@@ -2,11 +2,15 @@
 class User:
 
 
-    def __init__(self, user.id, username):
-        self.id  = user.id
+    def __init__(self, user_id, username):
+        self.id  = user_id
         self.username = username
+        self.followers = 0
     
-user_1 = User() 
+user_1 = User("001", "angela")
+user_2 = User("002", "Jack")
 
-print(user_1.username)
+print(user_1.followers)
+
+
 
