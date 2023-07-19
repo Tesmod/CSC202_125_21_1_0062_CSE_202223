fbd9c9694f9c84e389ce4a24cc964b4cd57cb4e9
Tesmod <agboolafarouq11@gmail.com>
2023-07-19 19:48:45 +0100

    Finished the snake game

--- Chunin/Week_three/Day20.py
+++ Chunin/Week_three/Day20.py
@@ -31,11 +31,19 @@ while game_is_on:
     #Detect collision with food
     if snake.head.distance(food) < 10:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     #Detect collision with wall.
     if snake.head.xcor() > 288 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.xcor() < -280:
         game_is_on = False
         scoreboard.game_over()
+
+    #Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+    
 
 screen.exitonclick()
