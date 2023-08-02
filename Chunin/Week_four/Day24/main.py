7e4accb5f68d90cb1d3904d2a4e5d2681be51e6e
Tesmod <agboolafarouq11@gmail.com>
2023-08-02 17:51:19 +0100

    Began work on the Mail Merge Challenge

--- Chunin/Week_four/Day24/main.py
+++ Chunin/Week_four/Day24/main.py
@@ -1,7 +1,4 @@
-with open("my_file.txt") as file:
+with open("../../../../../Desktop/my_file.txt") as file:
     contents = file.read()
     print(contents)
 
-
-with open("new_file.txt", mode="w") as file:
-    file.write("\nNew text.") 
