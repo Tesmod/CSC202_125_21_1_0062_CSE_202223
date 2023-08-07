34627a2aa8759571f4c5d224109d39c5be38ee6f
Tesmod <agboolafarouq11@gmail.com>
2023-08-07 14:10:06 +0100

    Completed the first part of the U.S. States Game

--- Chunin/Week_four/Day25/two.py
+++ Chunin/Week_four/Day25/two.py
@@ -1,3 +1,11 @@
 #U.S. States Game
 import turtle
-screen =
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_state_img.gif"
+screen.addshape("image")
+turtle.shape(image)
+
+
+answer_state = screen.textinput(title="Guess the State", prompt= "What is another State's name?")
+print(answer_state)
