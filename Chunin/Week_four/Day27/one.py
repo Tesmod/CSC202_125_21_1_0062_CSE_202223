8813b03e78e23e9f5ce850312c99a6b49c320f01
Tesmod <agboolafarouq11@gmail.com>
2023-08-11 06:22:47 +0100

    Completed the miles to kilometer project, hence completes Day27

--- Chunin/Week_four/Day27/one.py
+++ Chunin/Week_four/Day27/one.py
@@ -5,12 +5,13 @@ import tkinter
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 #Label
 
 my_label = Label(text="I am a Label", font=("Arial", 24, "italic"))
-my_label.pack()
-
-my_label["text"] = "New Text"
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
 
 
 #Button
@@ -19,15 +20,16 @@ def button_clicked():
     print("I got clicked")
     new_text = input.get()
     my_label.config(text=new_text)
-
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+new_button = Button(text="New button")
+new_button.grid(column=2, row=0)
 
 #Entry
 input = Entry(width=10)
-input.pack()
 print(input.get())
-
+input.grid(column=3, row=2)
 
 
 
